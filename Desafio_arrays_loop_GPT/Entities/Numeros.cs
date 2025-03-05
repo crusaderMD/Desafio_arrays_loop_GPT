a353d4aa803e690596893c154411521995c9755d
Luiz R. Bortolotti Jr. <luiz_bortolotti@hotmial.com>
2025-03-05 13:28:29 -0300

    Versao POO, usando encapsulamento e abstracao

---
+++ Desafio_arrays_loop_GPT/Entities/Numeros.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_arrays_loop_GPT.Entities
+{
+    public class Numeros
+    {
+        public int[] Numero { get; private set; }
+
+        public Numeros(int tamanho)
+        {
+            if (tamanho <= 0)
+            {
+                throw new ArgumentException("O tamanho do array deve ser maior que zero");
+            }
+            Numero = new int[tamanho];
+        }
+
+        public void InserirNumeros(int numero, int indice)
+        {
+            if (indice < 0 || indice >= Numero.Length)
+            {
+                throw new IndexOutOfRangeException("Indice Inválido");
+            }
+
+            Numero[indice] = numero;
+        }
+
+        public int MaiorNumero()
+        {
+            if (Numero == null || Numero.Length == 0)
+            {
+                throw new NullReferenceException("O array não foi inicializado");
+            }
+            return Numero.Max();
+        }
+
+        public int MenorNumero()
+        {
+            if (Numero == null || Numero.Length == 0)
+            {
+                throw new NullReferenceException("O array não foi inicializado");
+            }
+            return Numero.Min();
+        }
+
+        public double MediaNumeros()
+        {
+            if (Numero == null || Numero.Length == 0)
+            {
+                throw new NullReferenceException("O array não foi inicializado");
+            }
+            return Numero.Average();
+        }
+    }
+}
--- Desafio_arrays_loop_GPT/Program.cs
+++ Desafio_arrays_loop_GPT/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace arrayloops    {
+﻿using Desafio_arrays_loop_GPT.Entities;
+namespace arrayloops    {
     public class Program
     {
         public static void Main(string[] args)
@@ -12,75 +13,65 @@
             Extra: Permita ao usuário escolher quantos números ele quer inserir.
             */
 
-            // Versão Procedural
-            int[] numeros = new int[1];            
+            // Versão Orientada a Objetos
+            Numeros numeros = new(1);
+            bool valido = false;
             int quantidade = 0;
             int counter = 0;
-            bool valido = true;
+            int maior = 0;
+            int menor = 0;
+            double media = 0;
 
-            while (valido)
+            try
             {
-                Console.Write("Deseja inserir quantos números inteiros: ");
-                if (int.TryParse(Console.ReadLine(), out quantidade))
+                while (!valido)
                 {
-                    numeros = new int[quantidade];
-                    valido = false;
-                }
-                else
-                {
-                    Console.WriteLine("Não foi digitado um número válido");         
-                }                
-            }
-
-            while (quantidade != counter) {
-                Console.Write("Insira o {0}º número: ", counter + 1);
-                int numero;
-
-                if(int.TryParse(Console.ReadLine(), out numero))
-                {
-                    numeros[counter] = numero;
-                    counter++;
-                }
-                else
-                {
-                    Console.WriteLine("Não foi digitado um número válido");
-                }               
-            }
-
-            Console.WriteLine();
-
-            int menor = numeros[0];
-            int maior = numeros[0];
-            for (int i =0; i < numeros.Length; i++)            {
-                
-                if (numeros[i] != 0)                {
-                    
-                    if (numeros[i] > maior)
+                    Console.Write("Deseja inserir quantos números inteiros: ");
+                    if (int.TryParse(Console.ReadLine(), out quantidade))
                     {
-                        maior = numeros[i];
+                        numeros = new Numeros(quantidade);
+                        valido = true;
                     }
-                    if (numeros[i] < menor)
+                    else
                     {
-                        menor = numeros[i];
+                        Console.WriteLine("Não foi digitado um número válido");
                     }
                 }
+
+                while (quantidade != counter)
+                {
+                    Console.Write("Insira o {0}º número: ", counter + 1);
+                    int numero;
+
+                    if (int.TryParse(Console.ReadLine(), out numero))
+                    {
+                        numeros.InserirNumeros(numero, counter);
+                        counter++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Não foi digitado um número válido");
+                    }
+                }
+                Console.WriteLine();
+
+                maior = numeros.MaiorNumero();
+                menor = numeros.MenorNumero();
+
+                Console.WriteLine("O número maior é: {0}", maior);
+                Console.WriteLine();
+
+                Console.WriteLine("O número menor é: {0}", menor);
+                Console.WriteLine();
+
+                media = numeros.MediaNumeros();
+
+                Console.WriteLine("A média dos números é: {0}", media);
             }
-
-            Console.WriteLine("O maior número é: {0}", maior);
-            Console.WriteLine();          
-
-            Console.WriteLine("O menor número é: {0}", menor);
-            Console.WriteLine();
-
-            int soma = 0;            
-            for (int i = 0; i < numeros.Length; i++)
+            catch (ArgumentException e)
             {
-                soma += numeros[i];
+                Console.WriteLine(e.Message);
             }
-
-            double media = numeros.Length > 0 ? (double)soma / numeros.Length : 0;
-
-            Console.WriteLine("A média da soma dos números é: {0}", media);
         }
     }
 }
